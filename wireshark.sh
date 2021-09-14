c8c3d1217986ea1b8d3369881b7a7ba1142395d7
UmedaTakefumi <ut+github@azurestone.net>
2021-09-15 01:55:36 +0900

    add comment

--- wireshark.sh
+++ wireshark.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+<<OVERVIEW
+
+## パケットキャプチャスクリプト
+
+* パケットキャプチャするときに以下の作業をやってくれるスクリプトです
+  * パケットファイルを保存するディレクトリを作成します。
+  * パケットファイルのファイル名に実行日時を付与します。
+  * パケットファイルのローテーションの設定を行います。
+
+* 以前Qiitaに記載した記事をスクリプト化しました。
+* Qiita記事URL: https://qiita.com/UmedaTakefumi/items/d27edbef7a5475157a98
+
+OVERVIVE
+
 ## networksetup -listallhardwareports | grep Device: | awk '{ print $2 }'
 
 brew install wireshark
