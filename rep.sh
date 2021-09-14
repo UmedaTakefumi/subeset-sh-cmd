50a5c2618ddfb705be2b96e071b688cfbf0844b7
UmedaTakefumi <ut+github@azurestone.net>
2021-09-14 21:13:54 +0900

    fix print messeage

--- rep.sh
+++ rep.sh
@@ -2,5 +2,11 @@
 
 ## レポジトリチェックスクリプト
 
+check_dirname=$(pwd | awk -F/ '{ print $NF }')
+date=$(LANG=C date)
+
+echo -e "# -- $date --"
+echo -e "# check repository DIR:$check_dirname\n"
+
 find . -type d -name .git -prune -o -type f -exec file {} \; | \
 awk -F: '{ print $2 }' | env LANG=c sort | uniq -c | env LANG=c sort -nr
