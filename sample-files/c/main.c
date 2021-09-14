1c2059e3dd9071ba982fb72c0c13456722e98695
UmedaTakefumi <ut+github@azurestone.net>
2021-09-14 22:40:52 +0900

    add sample-files and build-text

---
+++ sample-files/c/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int hoge_func();
+
+int main() {
+    hoge_func();
+}
+
---
+++ sample-files/c/shared-lib-mac.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int hoge_func() {
+    printf("This is a shared library.\n");
+    return 0;
+}
