2a5e391d46786f30b0cc63eba9fb713eb8e2e66c
UmedaTakefumi <ut+github@azurestone.net>
2021-09-14 21:21:13 +0900

    add-comment

--- rep.sh
+++ rep.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+<<OVERVIEW
+
+## レポジトリチェックスクリプト
+
+* リポジトリディレクトリ内部のファイル構造を簡易的に集計し画面に出力するシェルスクリプトです。
+
+* 以前Qiitaに記載した記事をスクリプト化しました。
+* Qiita記事URL: https://qiita.com/UmedaTakefumi/items/4947fa2494a1f726bc2b
+
+OVERVIEW
+
 ## レポジトリチェックスクリプト
 
 check_dirname=$(pwd | awk -F/ '{ print $NF }')
