b05dbe727ba58f0db9949df8b37a75f3c9b2341e
UmedaTakefumi <ut+github@azurestone.net>
2021-09-17 02:51:14 +0900

    add cmd-opt

--- decoy-cmd.sh
+++ decoy-cmd.sh
@@ -18,4 +18,43 @@ function harddecoy () {
 
 }
 
+## copy and paste
+## 
+## ref: https://qiita.com/b4b4r07/items/dcd6be0bb9c9185475bb
+for OPT in "$@"
+do
+  case $OPT in
+    -h | --help)
+        print_header
+        print_how_to_use
+        exit 1
+        ;;
+    -s | --softdecoy)
+        print_header
+        softdecoy
+        shift 1
+        ;;
+    -- | -)
+        shift 1
+        param+=( "$@" )
+        break
+        ;;
+    -*)
+        echo "$PROGNAME: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+        exit 1
+        ;;
+    *)
+        if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
+            #param=( ${param[@]} "$1" )
+            param+=( "$1" )
+            shift 1
+        fi
+        ;;
+  esac
+done
 
+if [ -z "$param" ]; then
+    echo "$PROGNAME: too few arguments" 1>&2
+    echo "Try '$PROGNAME --help' for more information." 1>&2
+    exit 1
+fi
