64c1f679ada97b53973ca08509c6cbad66f85a2a
UmedaTakefumi <ut+github@azurestone.net>
2021-09-17 02:45:18 +0900

    🎣 success

---
+++ tree.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## copy and paste code.
+##
+## ref: https://qiita.com/yone098@github/items/bba8a42de6b06e40983b
+
+pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
