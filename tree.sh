2f468895b4030dba92dde602a83502e0bacd807f
UmedaTakefumi <ut+github@azurestone.net>
2021-09-17 03:01:47 +0900

    add comment

--- tree.sh
+++ tree.sh
@@ -5,3 +5,6 @@
 ## ref: https://qiita.com/yone098@github/items/bba8a42de6b06e40983b
 
 pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
+
+# brew install tree
+# dnf -y install tree
